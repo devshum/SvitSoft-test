ee88d36fd1915df25a7d2d2f0a1fd74e7e07c1b1
devshum <57689318+devshum@users.noreply.github.com>
2020-10-19 21:03:42 +0300

    refactoring, added an IIFE pattern module

--- js/app.js
+++ js/app.js
@@ -1,42 +1,147 @@
-const header = document.getElementById('header-js');
-const mainBox = document.getElementById('main-box-js');
-const navigation = document.getElementById('navigation-js');
+const dataController = (function() {
+    const state = {
+        navigation: { toggled: false },
 
-const figureNoneAll = Array.from(document.querySelectorAll('.figure-none-js'));
-const linksAll = Array.from(document.querySelectorAll('.link-js'));
+        slider: { currentPage: 1 }
+    };
 
-const cellIcon = document.getElementById('cell-icon-js');
+    return {
+        isToggled() { return state.navigation.toggled; },
+        
+        toggleState() { state.navigation.toggled = !state.navigation.toggled; },
 
-const crossLeft = document.getElementById('cross-left-js');
-const crossRight = document.getElementById('cross-right-js');
+        getState() { return state; }
+    }
+})();
 
-const topFigure = document.getElementById('top-js');
-const rightFigure = document.getElementById('right-js');
-const bottomFigure = document.getElementById('bottom-js');
-const leftFigure = document.getElementById('left-js');
+const UIController = (function() {
+    const DOMStrings = {
+        header: 'header-js',
+        mainBox: 'main-box-js',
+        navigation: 'navigation-js',
+        figureNone: '.figure-none-js',
+        links: '.link-js',
+        cellIcon: 'cell-icon-js',
+        crossLeft: 'cross-left-js',
+        crossRight: 'cross-right-js',
+        topFigure: 'top-js',
+        rightFigure: 'right-js',
+        bottomFigure: 'bottom-js',
+        leftFigure: 'left-js'
+    };
 
-cellIcon.addEventListener('click', () => {
-    // ICON ANIMATION
-    figureNoneAll.forEach(figure => figure.classList.toggle('animation-cross-none-js'));
+    return {
+        getDOMStrings() { return DOMStrings; },
 
-    crossLeft.classList.toggle('animation-cross-left-js');
-    crossRight.classList.toggle('animation-cross-right-js');
+        hideFigures() {
+            // HIDE THE FIGURES
+            let figureNoneAll = document.querySelectorAll(DOMStrings.figureNone);
+            figureNoneAll = Array.from(figureNoneAll);
 
-    topFigure.classList.toggle('animation-top-js');
-    rightFigure.classList.toggle('animation-right-js');
-    bottomFigure.classList.toggle('animation-bottom-js');
-    leftFigure.classList.toggle('animation-left-js');
+            figureNoneAll.forEach(figure => figure.classList.toggle('animation-cross-none-js'));
+        },
 
-    // TOGGLE NAVIGATION
-    navigation.classList.toggle('toggle-navigation-js');
-    mainBox.classList.toggle('toggle-main-box-js');
-    header.classList.toggle('toggle-header-js');
+        crossFigures() {
+            // CROSS THE FIGURES
+            const crossLeft = document.getElementById(DOMStrings.crossLeft);
+            const crossRight = document.getElementById(DOMStrings.crossRight);
 
-    linksAll.forEach((link, i) =>{
-        if(link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation=`toggleLink 1s ease forwards ${i / 4 + 0.4}s`;
+            crossLeft.classList.toggle('animation-cross-left-js');
+            crossRight.classList.toggle('animation-cross-right-js');
+        },
+
+        transformFigures() {
+            // TRANSFORM THE FIGURES
+            const topFigure = document.getElementById(DOMStrings.topFigure);
+            const rightFigure = document.getElementById(DOMStrings.rightFigure);
+            const bottomFigure = document.getElementById(DOMStrings.bottomFigure);
+            const leftFigure = document.getElementById(DOMStrings.leftFigure);
+
+            topFigure.classList.toggle('animation-top-js');
+            rightFigure.classList.toggle('animation-right-js');
+            bottomFigure.classList.toggle('animation-bottom-js');
+            leftFigure.classList.toggle('animation-left-js');
+        },
+
+        animateHeaderComponents() {
+            const header = document.getElementById(DOMStrings.header);
+            const mainBox = document.getElementById(DOMStrings.mainBox);
+
+            mainBox.classList.toggle('toggle-main-box-js');
+            header.classList.toggle('toggle-header-js');
+        },
+
+        animateNavigation() {
+            const navigation = document.getElementById(DOMStrings.navigation);
+
+            navigation.classList.toggle('toggle-navigation-js');
+        },
+
+        animateLinks() {
+            let linksAll = document.querySelectorAll(DOMStrings.links);
+            linksAll = Array.from(linksAll);
+
+            linksAll.forEach((link, i) =>{
+                if(link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation=`toggleLink 1s ease forwards ${i / 4 + 0.4}s`;
+                }
+            });
         }
-    });
-});
+    };
+})();
+
+const globalController = (function(dataCtrl, UICtrl) {
+
+    const iconAnimationCtrl = () => {
+        // TOGGLE THE STATE VALUE
+        dataCtrl.toggleState();
+
+        // HIDE THE FIGURES
+        UICtrl.hideFigures();
+    
+        // CROSS THE FIGURES
+        UICtrl.crossFigures();
+        
+        // TRANSFORM THE FIGURES
+        UICtrl.transformFigures();
+    };
+
+    const navigationAnimationCtrl = () => {
+        // ANIMATE HEADER COMPONENTS
+        UICtrl.animateHeaderComponents();
+
+        // ANIMATE THE NAV
+        UICtrl.animateNavigation();
+
+        // ANIMATE THE LINKS()
+        UICtrl.animateLinks();
+    };
+
+    const toggleNavigation = () => {
+        // TOGGLE THE STATE VALUE
+        dataCtrl.toggleState();
+
+        // GET TOGGLED VALUE FROM THE STATE
+        const isToggled = dataCtrl.isToggled();
+
+        // CHECK IS TOGGLED
+        if(isToggled) {
+            iconAnimationCtrl();
+            navigationAnimationCtrl();
+        };
+    };
+
+    const setupEventListeners = () => {
+        const DOMStrings = UICtrl.getDOMStrings();
+        document.getElementById(DOMStrings.cellIcon).addEventListener('click', toggleNavigation);
+    };
+
+    return {
+        init() { setupEventListeners() }
+    }
+
+})(dataController, UIController);
+
+globalController.init();
